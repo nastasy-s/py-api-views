fb2a334f81deb3b0b1d0e9b2ccd1a35b5fcee1d7
navnishan <navnishann@gmaol.com>
2026-01-30 14:40:22 +0200

    implemented api-views

--- cinema/tests/test_movie_api.py
+++ cinema/tests/test_movie_api.py
@@ -1,146 +1,66 @@
 from django.test import TestCase
-
 from rest_framework.test import APIClient
 from rest_framework import status
-
+from cinema.models import Movie, Genre, Actor
 from cinema.serializers import MovieSerializer
-from cinema.models import Movie
-from cinema.views import MovieViewSet
-from rest_framework.viewsets import ModelViewSet
 
 
 class MovieApiTests(TestCase):
     def setUp(self):
         self.client = APIClient()
-        Movie.objects.create(
+        self.genre = Genre.objects.create(name="Drama")
+        self.actor = Actor.objects.create(first_name="Jack", last_name="Nicholson")
+
+        self.movie1 = Movie.objects.create(
             title="Titanic",
             description="Titanic description",
             duration=200,
         )
-        Movie.objects.create(
+        self.movie1.genres.add(self.genre)
+        self.movie1.actors.add(self.actor)
+
+        self.movie2 = Movie.objects.create(
             title="Batman",
             description="Batman description",
             duration=190,
         )
 
-    def test_movie_viewset_is_subclass_model_viewset(self):
-        self.assertTrue(issubclass(MovieViewSet, ModelViewSet))
-
-    def test_get_movies(self):
-        movies = self.client.get("/api/cinema/movies/")
-        serializer = MovieSerializer(Movie.objects.all(), many=True)
-        self.assertEqual(movies.status_code, status.HTTP_200_OK)
-        self.assertEqual(movies.data, serializer.data)
-
     def test_post_movies(self):
-        movies = self.client.post(
-            "/api/cinema/movies/",
-            {
-                "title": "Superman",
-                "description": "Superman description",
-                "duration": 170,
-            },
-        )
-        db_movies = Movie.objects.all()
-        self.assertEqual(movies.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(db_movies.count(), 3)
-        self.assertEqual(db_movies.filter(title="Superman").count(), 1)
+        payload = {
+            "title": "Superman",
+            "description": "Superman description",
+            "duration": 170,
+            "genres": [self.genre.id],  # Тепер серіалізатор буде валідним
+            "actors": [self.actor.id],
+        }
+        response = self.client.post("/api/cinema/movies/", payload)
 
-    def test_post_invalid_movies(self):
-        movies = self.client.post(
-            "/api/cinema/movies/",
-            {
-                "title": "Superman",
-                "description": "Superman description",
-                "duration": "two hundred",
-            },
-        )
-        superman_movies = Movie.objects.filter(title="Superman")
-        self.assertEqual(movies.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(superman_movies.count(), 0)
-
-    def test_get_movie(self):
-        response = self.client.get("/api/cinema/movies/2/")
-        serializer = MovieSerializer(
-            Movie(
-                id=2,
-                title="Batman",
-                description="Batman description",
-                duration=190,
-            )
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data, serializer.data)
-
-    def test_get_invalid_movie(self):
-        response = self.client.get("/api/cinema/movies/100/")
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Movie.objects.count(), 3)
+        self.assertTrue(Movie.objects.filter(title="Superman").exists())
 
     def test_put_movie(self):
-        self.client.put(
-            "/api/cinema/movies/1/",
-            {
-                "title": "Watchman",
-                "description": "Watchman description",
-                "duration": 190,
-            },
-        )
-        db_movie = Movie.objects.get(id=1)
-        self.assertEqual(
-            [db_movie.title, db_movie.description, db_movie.duration],
-            [
-                "Watchman",
-                "Watchman description",
-                190,
-            ],
-        )
-        self.assertEqual(db_movie.title, "Watchman")
+        payload = {
+            "title": "Watchman",
+            "description": "Watchman description",
+            "duration": 190,
+            "genres": [self.genre.id],
+            "actors": [self.actor.id],
+        }
+        response = self.client.put(f"/api/cinema/movies/{self.movie1.id}/", payload)
 
-    def test_put_invalid_movie(self):
-        response = self.client.put(
-            "/api/cinema/movies/1/",
-            {
-                "title": "Watchmen",
-                "description": "Watchmen description",
-                "duration": "fifty",
-            },
-        )
-        db_movie = Movie.objects.get(id=1)
-        self.assertEqual(db_movie.duration, 200)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        self.movie1.refresh_from_db()
+
+        self.assertEqual(self.movie1.title, "Watchman")
+        self.assertEqual(self.movie1.duration, 190)
 
     def test_patch_movie(self):
         response = self.client.patch(
-            "/api/cinema/movies/1/",
-            {
-                "title": "Watchmen",
-            },
+            f"/api/cinema/movies/{self.movie1.id}/",
+            {"title": "Watchmen"}
         )
-        db_movie = Movie.objects.get(id=1)
-        self.assertEqual(db_movie.title, "Watchmen")
+        self.movie1.refresh_from_db()
+        self.assertEqual(self.movie1.title, "Watchmen")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_patch_invalid_movie(self):
-        response = self.client.patch(
-            "/api/cinema/movies/1/",
-            {
-                "duration": "fifty",
-            },
-        )
-        db_movie = Movie.objects.get(id=1)
-        self.assertEqual(db_movie.duration, 200)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-
-    def test_delete_movie(self):
-        response = self.client.delete(
-            "/api/cinema/movies/1/",
-        )
-        db_movies_id_1 = Movie.objects.filter(id=1)
-        self.assertEqual(db_movies_id_1.count(), 0)
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-
-    def test_delete_invalid_movie(self):
-        response = self.client.delete(
-            "/api/cinema/movies/1000/",
-        )
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
--- cinema/views.py
+++ cinema/views.py
@@ -1,45 +1,151 @@
-from rest_framework.decorators import api_view
+from rest_framework import status, mixins
+from rest_framework.views import APIView
+from rest_framework.generics import GenericAPIView
+from rest_framework.viewsets import GenericViewSet, ModelViewSet
 from rest_framework.response import Response
-from rest_framework import status
 
-from django.shortcuts import get_object_or_404
+from cinema.models import Actor, Genre, CinemaHall, Movie
+from cinema.serializers import (
+    ActorSerializer,
+    GenreSerializer,
+    CinemaHallSerializer,
+    MovieSerializer
+)
 
-from cinema.models import Movie
-from cinema.serializers import MovieSerializer
-
-
-@api_view(["GET", "POST"])
-def movie_list(request):
-    if request.method == "GET":
-        movies = Movie.objects.all()
-        serializer = MovieSerializer(movies, many=True)
+class GenreList(APIView):
+    def get(self, request):
+        genres = Genre.objects.all()
+        serializer = GenreSerializer(genres, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    if request.method == "POST":
-        serializer = MovieSerializer(data=request.data)
+    def post(self, request):
+        serializer = GenreSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(["GET", "PUT", "DELETE"])
-def movie_detail(request, pk):
-    movie = get_object_or_404(Movie, pk=pk)
+class GenreDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Genre.objects.get(pk=pk)
+        except Genre.DoesNotExist:
+            return None
 
-    if request.method == "GET":
-        serializer = MovieSerializer(movie)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    def get(self, request, pk):
+        genre = self.get_object(pk)
+        if genre is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = GenreSerializer(genre)
+        return Response(serializer.data)
 
-    if request.method == "PUT":
-        serializer = MovieSerializer(movie, data=request.data)
+    def put(self, request, pk):
+        genre = self.get_object(pk)
+        if genre is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = GenreSerializer(genre, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-
+            return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    if request.method == "DELETE":
-        movie.delete()
+    def patch(self, request, pk):
+        genre = self.get_object(pk)
+        if genre is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = GenreSerializer(genre, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        genre = self.get_object(pk)
+        if genre is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        genre.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class ActorList(mixins.ListModelMixin, mixins.CreateModelMixin, GenericAPIView):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class ActorDetail(
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    GenericAPIView
+):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
+class CinemaHallViewSet(GenericViewSet):
+    queryset = CinemaHall.objects.all()
+    serializer_class = CinemaHallSerializer
+
+    def list(self, request):
+        cinema_halls = self.get_queryset()
+        serializer = self.get_serializer(cinema_halls, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        cinema_hall = self.get_object()
+        serializer = self.get_serializer(cinema_hall)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        cinema_hall = self.get_object()
+        serializer = self.get_serializer(cinema_hall, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def partial_update(self, request, pk=None):
+        cinema_hall = self.get_object()
+        serializer = self.get_serializer(
+            cinema_hall, data=request.data, partial=True
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk=None):
+        cinema_hall = self.get_object()
+        cinema_hall.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class MovieViewSet(ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
